3f40e4eb82fee433161a0dd7b750fae14f510c88
wifekatt <wifek.attia.2000@gmail.com>
2022-04-10 12:52:03 +0100

    changed

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 const mongoose = require ('mongoose');
 
- mongoose.connect('mongodb://127.0.0.1:27017/DBvote' , (err) => {
-   if(!err)
-   console.log('mongoDB connection succeded.');
-   else
-   console.log('error in DB connection : '+ JSON.stringify(err, undefined, 2));
- });
+ mongoose.connect('mongodb://127.0.0.1:27017/DBvote' , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+.then(() => {
+  console.log("connected to mongodb");
+})
+.catch((err) => {
+  console.log("failed to connect to MongoDB", err);
+});
 
  module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require ('cors');
+require("dotenv").config();
+const app = express();
+const routes = require("./routes");
 
 const { mongoose } = require('./db.js');
 
-
-
-var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 app.use(bodyParser.json());
+
+//list all of your routes here
+app.use("/api/electeur", routes.electeurRoute);
+app.use("/api/admin", routes.adminRoute);
+app.use("/api/condidat", routes.condidatRoute);
+app.use("/api/listCondidat", routes.listCondidatRoute);
+app.use("/api/PrivilegeAdmin", routes.PrivilegeAdminRoute);
+app.use("/api/vote", routes.voteRoute);
+
 
 app.listen(3000, () => console.log('server started at port : 3000'));
 
-app.use('/electeur', electeurController);
