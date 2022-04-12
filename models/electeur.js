055798bf5e499b633d4d8e84321feee4d417086a
wifekatt <wifek.attia.2000@gmail.com>
2022-04-13 00:45:37 +0100

    name

--- models/electeur.js
+++ models/electeur.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 var electeur = mongoose.model('electeur', {
 	ElecteurID: { type: String },
-	FullName: { type: String }, 
-	language: { type: String }, 
+	FullName: { type: String },
+	Email: { type: String }, 
+	telephone: { type: String },
 	CIN: { type: String }, 
 	Pid: { type: String }, 
 	Zip_code: { type: String },
 	Bank_code: { type: String },
-	Email: { type: String }, 
-	telephone: { type: String }
+	language: { type: String }
 });
 
 module.exports = { electeur };
